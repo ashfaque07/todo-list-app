e5475c88ecb6d7ffd8f402e6f4c5a66020e937a1
aqshaikh <ashfaque.shaikh@finicity.com>
2023-09-30 15:53:22 +0530

    made changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React from "react";
-import Todo from "./components/Todo";
+// import Todo from "./components/Todo";
 import TodoList from "./TodoList";
 
 function App() {
