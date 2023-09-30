8ed9cf2994ca19fb86c8ae83e04a39ef470e1a11
aqshaikh <ashfaque.shaikh@finicity.com>
2023-09-30 15:44:31 +0530

    changes

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,5 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
-import ListGroup from "react-bootstrap/ListGroup";
-import Heading from "./Heading";
-import TodoInput from "./TodoInput";
-
 function TodoList(props) {
   return (
     <li className="list-Item" index={props.id}>
